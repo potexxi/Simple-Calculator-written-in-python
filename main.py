52e0a337a10f4df06a072ba8f6d1e2fed1f8b86c
potexxi <maximilian.schreiter@outlook.at>
2024-10-13 15:11:43 +0200

    Second Commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 def addition():
     zahl1 = float(input('Geben Sie Summand 1 ein: '))
     zahl2 = float(input('Geben Sie Summand 2 ein: '))
-    print (f'Summe: {zahl1 + zahl2}')
+    print (f'Summe: {zahl1 + zahl2}\n')
 
 def subtraktion():
     zahl1 = float(input('Geben Sie den Minuend ein: '))
@@ -13,30 +13,35 @@ def subtraktion():
 def multiplikation():
     zahl1 = float(input('Geben Sie Faktor 1 ein: '))
     zahl2 = float(input('Geben Sie Faktor 2 ein: '))
-    print (f'Produkt: {zahl1 * zahl2}')
+    print (f'Produkt: {zahl1 * zahl2}\n')
 
 def division():
     zahl1 = float(input('Geben Sie den Dividend ein: '))
-    zahl2 = float(input('Geben Sie den Divisor ein: '))
-    print (f'Quotient: {zahl1 / zahl2}')
+    zahl2 = 0
+    while zahl2 == 0:
+        zahl2 = float(input('Geben Sie den Divisor ein: '))
+        if zahl2 == 0:
+            print(f'Division durch null is nicht moeglich.')
+
+    print(f'Quotient: {zahl1 / zahl2}\n')
 
 def potenzieren():
     zahl1 = float(input('Geben Sie die Basis ein: '))
     zahl2 = float(input('Geben Sie den Exponet ein: '))
-    print (f'Ergebnis: {zahl1 ** zahl2}')
+    print (f'Ergebnis: {zahl1 ** zahl2}\n')
 
 def sqrt():
     zahl1 = float(input('Geben Sie den Radikand ein: '))
-    print (f'Ergebnis: {math.sqrt(zahl1)}')
+    print (f'Ergebnis: {math.sqrt(zahl1)}\n')
 
 def cbrt():
     zahl1 = float(input('Geben Sie den Radikand ein: '))
-    print (f'Ergebnis: {math.cbrt(zahl1)}')
+    print (f'Ergebnis: {math.cbrt(zahl1)}\n')
 
 def main():
     while True:
         try:
-            select = int(input('Bitte wählen Sie aus -\n'
+            select = int(input('\nBitte wählen Sie aus -\n'
                                '1. Addition\n'
                                '2. Subtraktion\n'
                                '3. Multiplikation\n'
